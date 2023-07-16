e90b47c0e3b722b16c9ba1fb1b50be6cd88ef6ad
Yuan <isyuan@yeah.net>
2023-07-16 23:20:50 +0800

    config by lua

---
+++ nvim/lua/lua/plugins/theme.lua
@@ -0,0 +1,35 @@
+return {
+    {
+        "folke/tokyonight.nvim",
+        dependencies = {
+            "nvim-lualine/lualine.nvim",
+            "nvim-tree/nvim-web-devicons",
+            -- "utilyre/barbecue.nvim",
+            -- "SmiteshP/nvim-navic",
+        },
+        config = function()
+            vim.cmd[[colorscheme tokyonight-storm]]
+            require("lualine").setup({
+                options = {
+                    theme = 'tokyonight-storm',
+                    component_separators = { left = '', right = ''},
+                    section_separators = { left = '', right = ''},
+                },
+                sections = {
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {
+                        {
+                            'filename',
+                            path = 1,
+                        },
+                    },
+                    lualine_x = {'selectioncount', 'encoding'}
+                }
+            })
+        end
+    },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = true,
+    }
+}
