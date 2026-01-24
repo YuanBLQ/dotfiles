532af1a4b5ff1d3c93bbe8d674f402926177cd54
Yuan <isyuan@yeah.net>
2026-01-24 19:33:27 +0800

    scripts: add link scripts

---
+++ cmd/ln.sh
@@ -0,0 +1,24 @@
+# kitty
+ln -s ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/dotfiles/kitty/themes ~/.config/kitty/themes
+
+# fish
+ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+
+# tmux
+ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+
+# git
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/git/delta.themes.gitconfig ~/.delta.themes.gitconfig
+
+# nvim
+ln -s ~/dotfiles/nvim/lazy/base.vim ~/.config/nvim/base.vim
+ln -s ~/dotfiles/nvim/lazy/init.lua ~/.config/nvim/init.lua
+ln -s ~/dotfiles/nvim/lazy/lazy-lock.json ~/.config/nvim/lazy-lock.json
+ln -s ~/dotfiles/nvim/lazy/lsp ~/.config/nvim/lsp
+ln -s ~/dotfiles/nvim/lazy/lua ~/.config/nvim/lua
+
+
+# k9s
+ln -s ~/dotfiles/k9s/skins ~/.config/k9s/skins
